be2c4794f39951b18731664e5e84c878d8e0c3c6
Voltra <ludwig.guerin.98@gmx.fr>
2017-11-24 23:23:37 +0100

    Fixed a bugged that caused 99% of the files not to be on the repo

---
+++ projet/dev/webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require("webpack");
+
+module.exports = {
+    "context": __dirname + "/js",
+    "entry": {
+        "error_404": "./error/404/error_404",
+        "auth_login": "./auth/login/auth_login"
+    },
+    "output": {
+        "filename": "[name].bundle.js",
+        "path": __dirname + "/../public_html/assets/js"
+    },
+    "target": "web",
+    "module": {
+        "loaders": [
+            {
+                "test": /\.js$/,
+                "exclude": /(node_modules)/,
+                "loader": "babel-loader"
+            }
+        ],
+        "rules": [
+            {
+                "test": require.resolve("jquery"),
+                "use": [
+                    {
+                        "loader": "expose-loader",
+                        "options": "$"
+                    },
+                    {
+                        "loader": "expose-loader",
+                        "options": "jQuery"
+                    }
+                ]
+            }
+        ]
+    },
+    "stats": {
+        "colors": true
+    }
+
+};
